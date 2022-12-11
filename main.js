956359eda1449c38087c4eb5c4d2d7ba6466d112
Voldernn <nikitamm191103191103@gmail.com>
2022-12-11 20:42:00 +0300

    commit

--- main.js
+++ main.js
@@ -12,5 +12,5 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   document.querySelector('.adv').style.background = colors[slideIndex-1];
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); 
 }
